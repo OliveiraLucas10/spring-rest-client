905728e2a221f14f0a2fd78527108d7ddbb60c04
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-03 06:15:38 +0100

    added new package and pojos

--- src/main/java/com/oliveiralucaspro/springrestclient/SpringRestClientApplication.java
+++ src/main/java/com/oliveiralucaspro/springrestclient/SpringRestClientApplication.java
@@ -1,4 +1,4 @@
-package com.oliveiralucaspro;
+package com.oliveiralucaspro.springrestclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/oliveiralucaspro/springrestclient/SpringRestClientApplicationTests.java
+++ src/test/java/com/oliveiralucaspro/springrestclient/SpringRestClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.oliveiralucaspro;
+package com.oliveiralucaspro.springrestclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
