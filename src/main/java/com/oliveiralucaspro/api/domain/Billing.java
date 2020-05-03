fe2d7445ad7bd09e48c70207f71e0794c4bb1ef2
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-03 06:25:01 +0100

    refactor using lombock project

--- src/main/java/com/oliveiralucaspro/api/domain/Billing.java
+++ src/main/java/com/oliveiralucaspro/api/domain/Billing.java
@@ -5,27 +5,15 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Billing implements Serializable
-{
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Billing implements Serializable {
 
     private Card card;
-    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private Map<String, Object> additionalProperties = new HashMap<>();
     private final static long serialVersionUID = 6577338081290507077L;
-
-    public Card getCard() {
-        return card;
-    }
-
-    public void setCard(Card card) {
-        this.card = card;
-    }
-
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }
 
 }
