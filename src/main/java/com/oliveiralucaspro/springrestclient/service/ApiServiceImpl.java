7e7d079c844c07e0cf89edb43c16187f0b798213
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-03 08:36:30 +0100

    added uri builder

--- src/main/java/com/oliveiralucaspro/springrestclient/service/ApiServiceImpl.java
+++ src/main/java/com/oliveiralucaspro/springrestclient/service/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package com.oliveiralucaspro.springrestclient.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.oliveiralucaspro.api.domain.User;
 import com.oliveiralucaspro.api.domain.UserData;
@@ -11,12 +13,20 @@ import com.oliveiralucaspro.api.domain.UserData;
 @Service
 public class ApiServiceImpl implements ApiService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();;
+
+    private final String api_url;
+
+    public ApiServiceImpl(@Value("${api.url}") String api_url) {
+	this.api_url = api_url;
+    }
 
     @Override
     public List<User> getUsers(Integer limit) {
 
-	UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+	UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(api_url).queryParam("limit", limit);
+
+	UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 	return userData.getData();
     }
 }
